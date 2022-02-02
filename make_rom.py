eb23c09a04f2186c84511c88bd2bfac215e0e6e0
Jack O'Connor <oconnor663@gmail.com>
2022-02-02 18:53:56 -0500

    flash LEDs with the 65C022 IO controller

--- make_rom.py
+++ make_rom.py
@@ -4,6 +4,30 @@ from os import path
 import sys
 
 HERE = path.dirname(__file__)
+SIZE = 32768
+
+# fmt: off
+code = bytearray([
+    0xa9, 0xff,        # lda #$ff
+    0x8d, 0x02, 0x60,  # sta $6002
+
+    0xa9, 0x55,        # lda #$55
+    0x8d, 0x00, 0x60,  # sta $6000
+
+    0xa9, 0xaa,        # lda #$aa
+    0x8d, 0x00, 0x60,  # sta $6000
+
+    0x4c, 0x05, 0x80,  # jmp $8005
+])
+# fmt: on
+
+rom = bytearray([0xEA] * SIZE)
+
+rom[: len(code)] = code
+
+# reset vector
+rom[0x7FFC] = 0x00
+rom[0x7FFD] = 0x80
 
 with open(path.join(HERE, "rom.bin"), "wb") as f:
-    f.write(b"\xea" * 32768)
+    f.write(rom)
