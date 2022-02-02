95b3de6c1c546c83fb964d623ea76c2c1eb84d6a
Jack O'Connor <oconnor663@gmail.com>
2022-02-01 20:10:55 -0500

    trying to print address and data

--- arduino.ino
+++ arduino.ino
@@ -1,21 +1,45 @@
+const char ADDR[] = {22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52};
+const char DATA[] = {39, 41, 43, 45, 47, 49, 51, 53};
+#define CLOCK 2
+#define READ_WRITE 3
+
 void setup() {
-  pinMode(13, OUTPUT);
+  for (int n = 0; n < 16; n++) {
+    pinMode(ADDR[n], INPUT);
+  }
+  for (int n = 0; n < 8; n++) {
+    pinMode(DATA[n], INPUT);
+  }
+  pinMode(CLOCK, INPUT);
+  pinMode(READ_WRITE, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(CLOCK), onClock, RISING);
+
+  Serial.begin(57600);
 }
 
-void flash_num(int n) {
-  for (int i = 0; i < n; i++) {
-    digitalWrite(13, HIGH);
-    delay(100);
-    digitalWrite(13, LOW);
-    delay(100);
+void onClock() {
+  char output[15];
+
+  unsigned int address = 0;
+  for (int n = 0; n < 16; n++) {
+    int bit = digitalRead(ADDR[n] ? 1 : 0);
+    Serial.print(bit);
+    address = (address << 1) + bit;
   }
+
+  Serial.print("   ");
+
+  unsigned int data = 0;
+  for (int n = 0; n < 8; n++) {
+    int bit = digitalRead(DATA[n] ? 1 : 0);
+    Serial.print(bit);
+    data = (data << 1) + bit;
+  }
+
+  sprintf(output, "   %04x  %c %02x", address, digitalRead(READ_WRITE) ? 'r' : 'w', data);
+  Serial.println(output);
 }
 
 void loop() {
-  int n = 0;
-  while (1) {
-    flash_num(n + 1);
-    delay(1000);
-    n = (n + 1) % 4;
-  }
 }
