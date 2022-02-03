cb787bf8c7c20041abf34c3f452393a90f923596
Jack O'Connor <oconnor663@gmail.com>
2022-02-03 16:05:33 -0500

    rolled hello world

--- rom.s
+++ rom.s
@@ -10,218 +10,88 @@ RS = %00100000
   .org $8000
 
 reset:
-  lda #%11111111  ; port B to output
+  ldx #$ff        ; initialize the stack pointer
+  txs
+
+  lda #%11111111  ; all pins port B to output
   sta DDRB
 
   lda #%11100000  ; top 3 pins of port A to output
   sta DDRA
 
   lda #%00000001  ; clear the display
-  sta PORTB
-
-  lda #0          ; clear RS/RW/E
-  sta PORTA
-
-  lda #E          ; set E
-  sta PORTA
-
-  lda #0          ; clear RS/RW/E
-  sta PORTA
+  jsr lcd_instruction
 
   lda #%00111000  ; set 8-bit mode, 2-line display, 5x8 font
-  sta PORTB
-
-  lda #0          ; clear RS/RW/E
-  sta PORTA
-
-  lda #E          ; set E
-  sta PORTA
-
-  lda #0          ; clear RS/RW/E
-  sta PORTA
+  jsr lcd_instruction
 
   lda #%00001110  ; display on, cursor on, blink off
-  sta PORTB
-
-  lda #0          ; clear RS/RW/E
-  sta PORTA
-
-  lda #E          ; set E
-  sta PORTA
-
-  lda #0          ; clear RS/RW/E
-  sta PORTA
+  jsr lcd_instruction
 
   lda #%00000110  ; increment cursor, do not shift display
-  sta PORTB
-
-  lda #0          ; clear RS/RW/E
-  sta PORTA
-
-  lda #E          ; set E
-  sta PORTA
-
-  lda #0          ; clear RS/RW/E
-  sta PORTA
+  jsr lcd_instruction
 
   lda #"H"
-  sta PORTB
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
-
-  lda #(RS | E)   ; set RS and E
-  sta PORTA
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
+  jsr print_char
 
   lda #"e"
-  sta PORTB
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
-
-  lda #(RS | E)   ; set RS and E
-  sta PORTA
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
+  jsr print_char
 
   lda #"l"
-  sta PORTB
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
-
-  lda #(RS | E)   ; set RS and E
-  sta PORTA
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
+  jsr print_char
 
   lda #"l"
-  sta PORTB
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
-
-  lda #(RS | E)   ; set RS and E
-  sta PORTA
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
+  jsr print_char
 
   lda #"o"
-  sta PORTB
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
-
-  lda #(RS | E)   ; set RS and E
-  sta PORTA
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
+  jsr print_char
 
   lda #","
-  sta PORTB
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
-
-  lda #(RS | E)   ; set RS and E
-  sta PORTA
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
+  jsr print_char
 
   lda #" "
-  sta PORTB
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
-
-  lda #(RS | E)   ; set RS and E
-  sta PORTA
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
+  jsr print_char
 
   lda #"w"
-  sta PORTB
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
-
-  lda #(RS | E)   ; set RS and E
-  sta PORTA
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
+  jsr print_char
 
   lda #"o"
-  sta PORTB
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
-
-  lda #(RS | E)   ; set RS and E
-  sta PORTA
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
+  jsr print_char
 
   lda #"r"
-  sta PORTB
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
-
-  lda #(RS | E)   ; set RS and E
-  sta PORTA
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
+  jsr print_char
 
   lda #"l"
-  sta PORTB
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
-
-  lda #(RS | E)   ; set RS and E
-  sta PORTA
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
+  jsr print_char
 
   lda #"d"
-  sta PORTB
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
-
-  lda #(RS | E)   ; set RS and E
-  sta PORTA
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
+  jsr print_char
 
   lda #"!"
-  sta PORTB
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
-
-  lda #(RS | E)   ; set RS and E
-  sta PORTA
-
-  lda #RS         ; set RS, clear RW/E
-  sta PORTA
+  jsr print_char
 
 loop:
   jmp loop
+
+lcd_instruction:
+  sta PORTB
+  lda #0          ; clear RS/RW/E
+  sta PORTA
+  lda #E          ; set E
+  sta PORTA
+  lda #0          ; clear RS/RW/E
+  sta PORTA
+  rts
+
+print_char:
+  sta PORTB
+  lda #RS         ; set RS, clear RW/E
+  sta PORTA
+  lda #(RS | E)   ; set RS and E
+  sta PORTA
+  lda #RS         ; set RS, clear RW/E
+  sta PORTA
+  rts
 
   .org $fffc
   .word reset
