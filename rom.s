03d40e04ac5f71607b305a927213f9da6922a16b
Jack O'Connor <oconnor663@gmail.com>
2022-02-04 13:40:00 -0500

    a print function

--- rom.s
+++ rom.s
@@ -9,7 +9,7 @@ RS = %00100000
 
   .org $8000
 
-reset:
+main:
   ldx #$ff        ; initialize the stack pointer
   txs
 
@@ -31,21 +31,19 @@ reset:
   lda #%00000001  ; clear the display
   jsr lcd_instruction
 
-  ldx #0
-print:
-  lda message, x
-  beq loop
-  jsr print_char
-  inx
-  jmp print
+  ; write the string pointer to $0..2 and print it
+  lda #<message  ; lsb
+  sta $0
+  lda #>message  ; msb
+  sta $1
+  jsr print_str
 
-loop:
-  jmp loop
+end_loop:
+  jmp end_loop
 
 message: .asciiz "I love you                             |Boyang!!!"
 
 lcd_wait:
-  pha
   lda #%00000000  ; all pins port B to input
   sta DDRB
 lcdbusy:
@@ -61,11 +59,13 @@ lcdbusy:
   sta PORTA
   lda #%11111111  ; all pins port B to output
   sta DDRB
-  pla
   rts
 
+; reads A
 lcd_instruction:
+  pha
   jsr lcd_wait
+  pla
   sta PORTB
   lda #0          ; clear RS/RW/E
   sta PORTA
@@ -75,8 +75,24 @@ lcd_instruction:
   sta PORTA
   rts
 
+; str pointer at #0
+print_str:
+  ldy #0
+print_str_loop:
+  lda ($0), y
+  beq print_str_end
+  jsr pause
+  jsr print_char
+  iny
+  jmp print_str_loop
+print_str_end:
+  rts
+
+; reads A
 print_char:
+  pha
   jsr lcd_wait
+  pla
   sta PORTB
   lda #RS         ; set RS, clear RW/E
   sta PORTA
@@ -86,6 +102,38 @@ print_char:
   sta PORTA
   rts
 
+; saves A, X, and Y
+pause:
+  pha
+  txa
+  pha
+  tya
+  pha
+
+  ; tweak A here to adjust the pause
+  lda #1
+  ldx #0
+  ldy #0
+pause_loop:
+  ; decrement X
+  dex
+  bne pause_loop
+  ; if X reached 0, decrement Y
+  dey
+  bne pause_loop
+  ; if Y reached 0, decrement A
+  sec
+  sbc #1
+  bne pause_loop
+  ; if A reached 0, we're done
+
+  pla
+  tay
+  pla
+  tax
+  pla
+  rts
+
   .org $fffc
-  .word reset
+  .word main
   .word 0
